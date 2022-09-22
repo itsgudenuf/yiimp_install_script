2af14b77e913d42738f47f5ee96d502a26afbfcf
itsgudenuf <brian@walters.guru>
2022-09-22 07:07:13 -0500

    Started added things from DirtyHarryDev installer

--- install.sh
+++ install.sh
@@ -109,8 +109,8 @@
 
     hide_output sudo apt -y upgrade
     hide_output sudo apt -y autoremove
-    apt_install dialog python3 python3-pip acl nano apt-transport-https
-    apt_install figlet
+    apt_install dialog python3 python3-pip acl nano apt-transport-https figlet
+    
     echo -e "$GREEN Done...$COL_RESET"
     
     # Switch Aptitude
@@ -1383,6 +1383,33 @@ echo '
     cd ~
     echo -e "$GREEN Done...$COL_RESET"
 
+
+    # Set up stuff from Dirty Harry Yiimp installer
+    apt_install lsb-release figlet update-motd \
+        landscape-common update-notifier-common
+
+    cd $HOME/yiimp/ubuntu/etc/update-motd.d
+    sudo rm -r /etc/update-motd.d/
+    sudo mkdir /etc/update-motd.d/
+    sudo touch /etc/update-motd.d/00-header ; sudo touch /etc/update-motd.d/10-sysinfo ; sudo touch /etc/update-motd.d/90-footer
+    sudo chmod +x /etc/update-motd.d/*
+    sudo cp -r 00-header 10-sysinfo 90-footer /etc/update-motd.d/
+
+    echo '
+    clear
+    run-parts /etc/update-motd.d/ | sudo tee /etc/motd
+    ' | sudo -E tee /usr/bin/motd >/dev/null 2>&1
+
+    sudo chmod +x /usr/bin/motd
+
+
+    # copy additional files
+    cd $HOME/yiimp/ubuntu
+    sudo cp -r screens /usr/bin/
+    sudo chmod +x /usr/bin/screens
+
+
+    ##### End of Dirty Harry additions ### 
 
     # Final Directory permissions
     echo
