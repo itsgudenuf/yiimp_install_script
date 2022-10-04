ee33db08c595d8608a16542f76e28c61f00d9f95
itsgudenuf <brian@walters.guru>
2022-10-04 14:36:07 -0500

    removed ssl install option for now

--- install.sh
+++ install.sh
@@ -89,7 +89,6 @@
     read -e -p "Enter desired Yiimp GitHub (1=Kudaraidee or 2=tpruvot or 3=Afiniel-Tech 4= Afiniel) [1 by default] : " yiimpver
     read -e -p "Install Fail2ban? [y/N] : " install_fail2ban
     read -e -p "Install UFW and configure ports? [y/N] : " UFW
-    read -e -p "Install LetsEncrypt SSL? IMPORTANT! You MUST have your domain name pointed to this server prior to running the script!! [y/N]: " ssl_install
     read -e -p "Install Wireguard for future remote stratums??? [y/N]: " wg_install
     if [[ ("$wg_install" == "y" || "$wg_install" == "Y") ]]; then
         read -e -p "Enter a Local Wireguard Private IP for this server (x.x.x.x): " wg_ip
@@ -107,7 +106,6 @@
     echo "Yiimb Github choice: $yiimpver"
     echo "Install Fail2ban:    $install_fail2ban"
     echo "Install UFW:         $UFW"
-    echo "Install SSL now:     $ssl_install"
     echo "Install wiregauard:  $wg_install"
     echo "Wireguard wg0 IP:    $wg_ip"
 
@@ -637,138 +635,10 @@
 
         sudo ln -s /etc/nginx/sites-available/$server_name.conf /etc/nginx/sites-enabled/$server_name.conf
         sudo ln -s /var/web /var/www/$server_name/html
+ 
         hide_output sudo systemctl reload php7.3-fpm.service
         hide_output sudo systemctl restart nginx.service
         echo -e "$GREEN Done...$COL_RESET"
-            
-        if [[ ("$ssl_install" == "y" || "$ssl_install" == "Y") ]]; then
-
-
-            
-            # Install SSL (with SubDomain)
-            echo
-            echo -e "Install LetsEncrypt and setting SSL (with SubDomain)"
-            echo
-            
-            apt_install letsencrypt
-            sudo letsencrypt certonly -a webroot --webroot-path=/var/web --email "$EMAIL" --agree-tos -d "$server_name"
-            sudo rm /etc/nginx/sites-available/$server_name.conf
-            sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-            # I am SSL Man!
-            echo 'include /etc/nginx/blockuseragents.rules;
-            server {
-            if ($blockedagent) {
-                        return 403;
-                }
-                if ($request_method !~ ^(GET|HEAD|POST)$) {
-                return 444;
-                }
-                listen 80;
-                listen [::]:80;
-                server_name '"${server_name}"';
-                # enforce https
-                return 301 https://$server_name$request_uri;
-            }
-            
-            server {
-            if ($blockedagent) {
-                        return 403;
-                }
-                if ($request_method !~ ^(GET|HEAD|POST)$) {
-                return 444;
-                }
-                    listen 443 ssl http2;
-                    listen [::]:443 ssl http2;
-                    server_name '"${server_name}"';
-                
-                    root /var/www/'"${server_name}"'/html/web;
-                    index index.php;
-                
-                    access_log /var/log/nginx/'"${server_name}"'.app-access.log;
-                    error_log  /var/log/nginx/'"${server_name}"'.app-error.log;
-                
-                    # allow larger file uploads and longer script runtimes
-            client_body_buffer_size  50k;
-                client_header_buffer_size 50k;
-                client_max_body_size 50k;
-                large_client_header_buffers 2 50k;
-                sendfile off;
-                
-                    # strengthen ssl security
-                    ssl_certificate /etc/letsencrypt/live/'"${server_name}"'/fullchain.pem;
-                    ssl_certificate_key /etc/letsencrypt/live/'"${server_name}"'/privkey.pem;
-                    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-                    ssl_prefer_server_ciphers on;
-                    ssl_session_cache shared:SSL:10m;
-                    ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:ECDHE-RSA-AES128-GCM-SHA256:AES256+EECDH:DHE-RSA-AES128-GCM-SHA256:AES256+EDH:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4";
-                    ssl_dhparam /etc/ssl/certs/dhparam.pem;
-                
-                    # Add headers to serve security related headers
-                    add_header Strict-Transport-Security "max-age=15768000; preload;";
-                    add_header X-Content-Type-Options nosniff;
-                    add_header X-XSS-Protection "1; mode=block";
-                    add_header X-Robots-Tag none;
-                    add_header Content-Security-Policy "frame-ancestors 'self'";
-                
-                location / {
-                try_files $uri $uri/ /index.php?$args;
-                }
-                location @rewrite {
-                rewrite ^/(.*)$ /index.php?r=$1;
-                }
-            
-                
-                    location ~ ^/index\.php$ {
-                        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-                        fastcgi_pass unix:/var/run/php/php7.3-fpm.sock;
-                        fastcgi_index index.php;
-                        include fastcgi_params;
-                        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                        fastcgi_intercept_errors off;
-                        fastcgi_buffer_size 16k;
-                        fastcgi_buffers 4 16k;
-                        fastcgi_connect_timeout 300;
-                        fastcgi_send_timeout 300;
-                        fastcgi_read_timeout 300;
-                        include /etc/nginx/fastcgi_params;
-                    try_files $uri $uri/ =404;
-                }
-                location ~ \.php$ {
-                    return 404;
-                }
-                location ~ \.sh {
-                return 404;
-                }
-                
-                    location ~ /\.ht {
-                        deny all;
-                    }
-                location /phpmyadmin {
-                root /usr/share/;
-                index index.php;
-                try_files $uri $uri/ =404;
-                location ~ ^/phpmyadmin/(doc|sql|setup)/ {
-                    deny all;
-            }
-                location ~ /phpmyadmin/(.+\.php)$ {
-                    allow '"${Public}"';
-                    deny all;
-                    client_max_body_size 2M;
-                    fastcgi_pass unix:/run/php/php7.3-fpm.sock;
-                    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                    include fastcgi_params;
-                    include snippets/fastcgi-php.conf;
-            }
-            }
-            }
-                
-            ' | sudo -E tee /etc/nginx/sites-available/$server_name.conf >/dev/null 2>&1
-        fi
-        
-        hide_output sudo systemctl reload php7.3-fpm.service
-        hide_output sudo systemctl restart nginx.service
-        echo -e "$GREEN Done...$COL_RESET"
-	
 	
 	else
         echo 'include /etc/nginx/blockuseragents.rules;
@@ -854,136 +724,7 @@
 
         sudo ln -s /etc/nginx/sites-available/$server_name.conf /etc/nginx/sites-enabled/$server_name.conf
         sudo ln -s /var/web /var/www/$server_name/html
-        hide_output sudo systemctl reload php7.3-fpm.service
-        hide_output sudo systemctl restart nginx.service
-        echo -e "$GREEN Done...$COL_RESET"
-    
-        
-        if [[ ("$ssl_install" == "y" || "$ssl_install" == "Y") ]]; then
-            
-            # Install SSL (without SubDomain)
-            echo
-            echo -e "Install LetsEncrypt and setting SSL (without SubDomain)"
-            echo
-            sleep 3
-            
-            apt_install letsencrypt
-            sudo letsencrypt certonly -a webroot --webroot-path=/var/web --email "$EMAIL" --agree-tos -d "$server_name" -d www."$server_name"
-            sudo rm /etc/nginx/sites-available/$server_name.conf
-            sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-            # I am SSL Man!
-            echo 'include /etc/nginx/blockuseragents.rules;
-            server {
-            if ($blockedagent) {
-                        return 403;
-                }
-                if ($request_method !~ ^(GET|HEAD|POST)$) {
-                return 444;
-                }
-                listen 80;
-                listen [::]:80;
-                server_name '"${server_name}"';
-                # enforce https
-                return 301 https://$server_name$request_uri;
-            }
-            
-            server {
-            if ($blockedagent) {
-                        return 403;
-                }
-                if ($request_method !~ ^(GET|HEAD|POST)$) {
-                return 444;
-                }
-                    listen 443 ssl http2;
-                    listen [::]:443 ssl http2;
-                    server_name '"${server_name}"' www.'"${server_name}"';
-                
-                    root /var/www/'"${server_name}"'/html/web;
-                    index index.php;
-                
-                    access_log /var/log/nginx/'"${server_name}"'.app-access.log;
-                    error_log  /var/log/nginx/'"${server_name}"'.app-error.log;
-                
-                    # allow larger file uploads and longer script runtimes
-            client_body_buffer_size  50k;
-                client_header_buffer_size 50k;
-                client_max_body_size 50k;
-                large_client_header_buffers 2 50k;
-                sendfile off;
-                
-                    # strengthen ssl security
-                    ssl_certificate /etc/letsencrypt/live/'"${server_name}"'/fullchain.pem;
-                    ssl_certificate_key /etc/letsencrypt/live/'"${server_name}"'/privkey.pem;
-                    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
-                    ssl_prefer_server_ciphers on;
-                    ssl_session_cache shared:SSL:10m;
-                    ssl_ciphers "EECDH+AESGCM:EDH+AESGCM:ECDHE-RSA-AES128-GCM-SHA256:AES256+EECDH:DHE-RSA-AES128-GCM-SHA256:AES256+EDH:ECDHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-RSA-AES128-SHA256:ECDHE-RSA-AES256-SHA:ECDHE-RSA-AES128-SHA:DHE-RSA-AES256-SHA256:DHE-RSA-AES128-SHA256:DHE-RSA-AES256-SHA:DHE-RSA-AES128-SHA:ECDHE-RSA-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:AES256-GCM-SHA384:AES128-GCM-SHA256:AES256-SHA256:AES128-SHA256:AES256-SHA:AES128-SHA:DES-CBC3-SHA:HIGH:!aNULL:!eNULL:!EXPORT:!DES:!MD5:!PSK:!RC4";
-                    ssl_dhparam /etc/ssl/certs/dhparam.pem;
-                
-                    # Add headers to serve security related headers
-                    add_header Strict-Transport-Security "max-age=15768000; preload;";
-                    add_header X-Content-Type-Options nosniff;
-                    add_header X-XSS-Protection "1; mode=block";
-                    add_header X-Robots-Tag none;
-                    add_header Content-Security-Policy "frame-ancestors 'self'";
-                
-                location / {
-                try_files $uri $uri/ /index.php?$args;
-                }
-                location @rewrite {
-                rewrite ^/(.*)$ /index.php?r=$1;
-                }
-            
-                
-                    location ~ ^/index\.php$ {
-                        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-                        fastcgi_pass unix:/var/run/php/php7.3-fpm.sock;
-                        fastcgi_index index.php;
-                        include fastcgi_params;
-                        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                        fastcgi_intercept_errors off;
-                        fastcgi_buffer_size 16k;
-                        fastcgi_buffers 4 16k;
-                        fastcgi_connect_timeout 300;
-                        fastcgi_send_timeout 300;
-                        fastcgi_read_timeout 300;
-                        include /etc/nginx/fastcgi_params;
-                    try_files $uri $uri/ =404;
-                }
-                location ~ \.php$ {
-                    return 404;
-                }
-                location ~ \.sh {
-                return 404;
-                }
-                
-                    location ~ /\.ht {
-                        deny all;
-                    }
-                location /phpmyadmin {
-                root /usr/share/;
-                index index.php;
-                try_files $uri $uri/ =404;
-                location ~ ^/phpmyadmin/(doc|sql|setup)/ {
-                    deny all;
-            }
-                location ~ /phpmyadmin/(.+\.php)$ {
-                    allow '"${Public}"';
-                    deny all;
-                    client_max_body_size 2M;
-                    fastcgi_pass unix:/run/php/php7.3-fpm.sock;
-                    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-                    include fastcgi_params;
-                    include snippets/fastcgi-php.conf;
-                }
-            }
-            }
-                
-            ' | sudo -E tee /etc/nginx/sites-available/$server_name.conf >/dev/null 2>&1
 
-            echo -e "$GREEN Done...$COL_RESET"
-
-        fi
         hide_output sudo systemctl reload php7.3-fpm.service
         hide_output sudo systemctl restart nginx.service
     fi
